f2eafc9480fe80f1156895204cca133ce2099ed3
Sam Duckett <sduckett@dawsonstudents.org>
2024-02-25 10:00:35 -0700

    comp things

--- src/main/java/frc/robot/Mechanism.java
+++ src/main/java/frc/robot/Mechanism.java
@@ -3,11 +3,10 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.controls.MotionMagicDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.GravityTypeValue;
 
 import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -17,157 +16,178 @@ import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class Mechanism implements Subsystem {
-	// private boolean homed = false;
-	private final DigitalInput limit = new DigitalInput(0);
+    private boolean homed = false;
+    private final DigitalInput limit = new DigitalInput(0);
 
-	private final CommandXboxController operator; // My joystick
+    private final CommandXboxController operator; // My joystick
 
-	private DoubleSubscriber intakeSpeed = NetworkTableInstance.getDefault().getDoubleTopic("intakeSpeed")
-			.subscribe(0.6);
+    private DoubleSubscriber intakeSpeed = NetworkTableInstance.getDefault().getDoubleTopic("intakeSpeed")
+            .subscribe(0.6);
 
-	private DoubleSubscriber feederSpeed = NetworkTableInstance.getDefault().getDoubleTopic("feederSpeed")
-			.subscribe(-0.25);
+    private DoubleSubscriber feederSpeed = NetworkTableInstance.getDefault().getDoubleTopic("feederSpeed")
+            .subscribe(-0.25);
 
-	private DoubleSubscriber shooterSpeed = NetworkTableInstance.getDefault().getDoubleTopic("shooterSpeed")
-			.subscribe(-1);
+    private DoubleSubscriber shooterSpeed = NetworkTableInstance.getDefault().getDoubleTopic("shooterSpeed")
+            .subscribe(-1);
 
-	private DoubleSubscriber pivotSpeed = NetworkTableInstance.getDefault().getDoubleTopic("pivotSpeed")
-			.subscribe(0.15);
+    private DoubleSubscriber pivotSpeed = NetworkTableInstance.getDefault().getDoubleTopic("pivotSpeed")
+            .subscribe(0.4);
 
-	private double setpoint = -1;
+    private double setpoint = -1;
 
-	private TalonSRX intake;
-	private TalonSRX intake2;
+    private TalonSRX intake;
+    private TalonSRX intake2;
 
-	private TalonSRX feeder;
-	private TalonSRX shooter;
-	private TalonFX pivot;
+    private TalonSRX feeder;
+    private TalonSRX shooter;
+    private TalonFX pivot;
 
-	@SuppressWarnings("resource")
-	public Mechanism(CommandXboxController driver, CommandXboxController operator) {
-		register();
+    @SuppressWarnings("resource")
+    public Mechanism(CommandXboxController driver, CommandXboxController operator) {
+        register();
 
-		this.operator = operator;
+        this.operator = operator;
 
-		intake = new TalonSRX(9);
-		intake2 = new TalonSRX(10);
+        intake = new TalonSRX(9);
+        intake2 = new TalonSRX(10);
 
-		feeder = new TalonSRX(30);
+        feeder = new TalonSRX(30);
 
-		shooter = new TalonSRX(6);
-		new TalonSRX(20).set(TalonSRXControlMode.Follower, 6);
+        shooter = new TalonSRX(6);
+        new TalonSRX(20).set(TalonSRXControlMode.Follower, 6);
 
-		TalonFXConfiguration conf = new TalonFXConfiguration();
+        TalonFXConfiguration conf = new TalonFXConfiguration();
 
-		conf.Slot0.kP = 0.1 * 1;
-		conf.Slot0.kD = 0.000004 * 0.01 * 0;
+        // conf.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        conf.Feedback.SensorToMechanismRatio = 100;
 
-		conf.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-		conf.Feedback.SensorToMechanismRatio = 100;
+        conf.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+        conf.Slot0.kG = 0.0;
+        conf.Slot0.kP = 10;
 
-		pivot = new TalonFX(7);
-		TalonFX pivot2 = new TalonFX(17);
+        conf.MotionMagic.MotionMagicAcceleration = 0.2;
+        conf.MotionMagic.MotionMagicCruiseVelocity = 0.2;
 
-		pivot.getConfigurator().apply(conf);
-		pivot2.getConfigurator().apply(conf);
+        pivot = new TalonFX(7);
+        TalonFX pivot2 = new TalonFX(17);
 
-		pivot2.setControl(new Follower(7, true));
+        pivot.getConfigurator().apply(conf);
+        pivot2.getConfigurator().apply(conf);
 
-		operator.leftTrigger().onTrue(shooter(1)).onFalse(shooter(0));
-		operator.rightTrigger().onTrue(shooter(-1)).onFalse(shooter(0));
+        pivot2.setControl(new Follower(7, true));
 
-		operator.a().onTrue(feeder(1)).onFalse(feeder(0));
-		driver.a().onTrue(feeder(1).alongWith(intake(1))).onFalse(feeder(0).alongWith(intake(0)));
-		operator.b().onTrue(feeder(1).alongWith(intake(1))).onFalse(feeder(0).alongWith(intake(0)));
-		operator.x().onTrue(feeder(-1)).onFalse(feeder(0));
-		operator.y().onTrue(intake(-1)).onFalse(intake(0));
+        operator.leftTrigger().whileTrue(shooter(true));
+        // operator.rightTrigger().whileTrue(shooter(-1)).onFalse(shooter(0));
 
-		operator.start().onTrue(new InstantCommand(() -> pivot.setPosition(0)));
+        operator.rightTrigger().whileTrue(feeder(true));
+        operator.a().whileTrue(feeder(true).alongWith(intake(true)));
+        operator.b().whileTrue(feeder(false));
+        operator.y().whileTrue(intake(false));
 
-		operator.povDown().onTrue(pivot(0)).onFalse(pivotStop());
-		operator.povLeft().onTrue(pivot(0.1)).onFalse(pivotStop());
-	}
+        // operator.start().whileTrue(new InstantCommand(() -> pivot.setPosition(0)));
 
-	Command pivot(double pos) {
-		return new InstantCommand(() -> {
-			setpoint = pos;
-			pivot.setControl(new PositionDutyCycle(pos));
-		});
-		// .onlyIf(() -> homed);
-	}
+        operator.povDown().whileTrue(pivot(0.01));
+        operator.povLeft().whileTrue(pivot(0.028564453125));
+        operator.povRight().whileTrue(pivot(0.310302734375));
+    }
 
-	Command pivotStop() {
-		return new InstantCommand(() -> {
-			setpoint = -1;
-			pivot.set(0);
-		});
-		// .onlyIf(() -> homed);
-	}
+    Command pivot(double pos) {
+        return new FunctionalCommand(() -> {
+            setpoint = pos;
+            pivot.setControl(new MotionMagicDutyCycle(pos));
+        }, () -> {
+        }, (_interrupted) -> {
+            setpoint = -1;
+            pivot.set(0);
+        }, () -> Math.abs(pivot.getPosition().getValueAsDouble() - pos) < .005, this);
+        // .onlyIf(() -> homed);
+    }
 
-	/**
-	 * Sign should be -1, 0, or 1.
-	 */
-	Command feeder(int sign) {
-		return new InstantCommand(() -> feeder.set(TalonSRXControlMode.PercentOutput, sign * feederSpeed.get()));
-	}
+    Command feeder(boolean forwards) {
+        return new FunctionalCommand(() -> {
+            feeder.set(TalonSRXControlMode.PercentOutput, forwards ? feederSpeed.get() : -feederSpeed.get());
+        }, () -> {
+        }, (_interrupted) -> {
+            feeder.set(TalonSRXControlMode.PercentOutput, 0);
+        }, () -> false);
+    }
 
-	/**
-	 * Sign should be -1, 0, or 1.
-	 */
-	Command intake(int sign) {
-		return new InstantCommand(() -> {
-			double speed = intakeSpeed.get() * sign;
+    Command intake(boolean forwards) {
+        return new FunctionalCommand(() -> {
+            double speed = forwards ? intakeSpeed.get() : -intakeSpeed.get();
 
-			intake.set(TalonSRXControlMode.PercentOutput, speed);
-			intake2.set(TalonSRXControlMode.PercentOutput, -speed);
-		});
-	}
+            intake.set(TalonSRXControlMode.PercentOutput, speed);
+            intake2.set(TalonSRXControlMode.PercentOutput, -speed);
+        }, () -> {
+        }, (_interrupted) -> {
+            intake.set(TalonSRXControlMode.PercentOutput, 0);
+            intake2.set(TalonSRXControlMode.PercentOutput, 0);
+        }, () -> false);
+    }
 
-	/**
-	 * Sign should be -1, 0, or 1.
-	 */
-	public Command shooter(int sign) {
-		return new InstantCommand(() -> shooter.set(TalonSRXControlMode.PercentOutput, sign * shooterSpeed.get()));
-	}
+    Command shooter(boolean forwards) {
+        return new FunctionalCommand(() -> {
+            shooter.set(TalonSRXControlMode.PercentOutput, forwards ? shooterSpeed.get() : -shooterSpeed.get());
+        }, () -> {
+        }, (_interrupted) -> {
+            shooter.set(TalonSRXControlMode.PercentOutput, 0);
+        }, () -> false);
+    }
 
-	@Override
-	public void periodic() {
-		// if (!homed && limit.get()) {
-		// homed = true;
-		// pivot.set(0);
-		// pivot.setPosition(0);
-		// } else if (homed && !isSetpoint)
-		// pivot.set(operator.getLeftY() * pivotSpeed.get());
+    @Override
+    public void periodic() {
+        // if (!homed && limit.get()) {
+        // homed = true;
+        // pivot.set(0);
+        // pivot.setPosition(0);
+        // } else if (homed && !isSetpoint)
+        // pivot.set(operator.getLeftY() * pivotSpeed.get());
 
-		double pivotS = setpoint == -1 ? operator.getLeftY() * pivotSpeed.get() : setpoint == 0 ? -1 : 1;
+        // double pivotManual = -Math.round(operator.getRightY());
 
-		if ((pivotS < 0 && !limit.get()) || (pivotS > 0 && pivot.getPosition().getValueAsDouble() < 120 / 360))
-			if (setpoint == -1)
-				pivot.set(pivotS);
-			else
-				pivot.set(0);
+        // if (Math.abs(pivotManual) >= 0.1) {
+        // pivot.set(pivotManual * 0.0485);
+        // } else {
+        double pivotJoystick = -operator.getLeftY() * pivotSpeed.get();
 
-		if (limit.get())
-			pivot.setPosition(0);
+        if (setpoint != -1) {
+            if (limit.get() && setpoint == 0)
+                pivot.set(0);
+        } else {
+            if ((pivotJoystick < 0 && !limit.get())
+                    || (pivotJoystick > 0 && pivot.getPosition().getValueAsDouble() < 120.0 / 360.0))
+                pivot.set(pivotJoystick);
+            else
+                pivot.set(0);
+        }
 
-		SmartDashboard.putBoolean("Limit switch active", limit.get());
-		SmartDashboard.putNumber("pivot Pose", pivot.getPosition().asSupplier().get());
-		SmartDashboard.putNumber("pivot Velocity", pivot.getVelocity().asSupplier().get());
-		SmartDashboard.putNumber("pivot Acceleration", pivot.getAcceleration().asSupplier().get());
-		SmartDashboard.putNumber("pivot Current", pivot.getSupplyCurrent().asSupplier().get());
-		SmartDashboard.putNumber("pivot Volts", pivot.getSupplyVoltage().asSupplier().get());
-		SmartDashboard.putNumber("pivot Temp", pivot.getDeviceTemp().asSupplier().get());
-		piviott.setAngle(pivot.getPosition().getValueAsDouble() * 3.6);
-		SmartDashboard.putData("Piviot Mech", mech);
-	}
+        if (!homed && limit.get()) {
+            pivot.setPosition(0);
+            homed = true;
+        }
+        // }
 
-	// units are inches
-	private Mechanism2d mech = new Mechanism2d(28, 28);
-	private MechanismRoot2d root = mech.getRoot("PivPoint", 20, 18);
-	private MechanismLigament2d piviott = root.append(new MechanismLigament2d("Shooter", 18, 180));
+        SmartDashboard.putNumber("pivotS", pivotJoystick);
+        SmartDashboard.putBoolean("Limit switch active", limit.get());
+
+        SmartDashboard.putNumber("pivot Pose", pivot.getPosition().getValueAsDouble());
+        SmartDashboard.putNumber("pivot Velocity", pivot.getVelocity().asSupplier().get());
+        SmartDashboard.putNumber("pivot Acceleration", pivot.getAcceleration().asSupplier().get());
+        SmartDashboard.putNumber("pivot Current", pivot.getSupplyCurrent().asSupplier().get());
+        SmartDashboard.putNumber("pivot Volts", pivot.getSupplyVoltage().asSupplier().get());
+        SmartDashboard.putNumber("pivot Temp", pivot.getDeviceTemp().asSupplier().get());
+        piviott.setAngle(pivot.getPosition().getValueAsDouble() * 3.6);
+        SmartDashboard.putData("Piviot Mech", mech);
+    }
+
+    // units are inches
+    private Mechanism2d mech = new Mechanism2d(28, 28);
+    private MechanismRoot2d root = mech.getRoot("PivPoint", 20, 18);
+    private MechanismLigament2d piviott = root.append(new MechanismLigament2d("Shooter", 18, 180));
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,8 @@ package frc.robot;
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -14,79 +16,89 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.generated.TunerConstants;
 
 public class RobotContainer {
-	private double MaxSpeed = TunerConstants.kSpeedAt12VoltsMps; // kSpeedAt12VoltsMps desired top speed
-	private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
+    private double MaxSpeed = TunerConstants.kSpeedAt12VoltsMps; // kSpeedAt12VoltsMps desired top speed
+    private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
 
-	private Pose2d speakerPose = new Pose2d();
+    private Pose2d speakerPose = new Pose2d();
 
-	/* Setting up bindings for necessary control of the swerve drive platform */
-	private final CommandXboxController driver = new CommandXboxController(0); // My joystick
-	private final CommandXboxController operator = new CommandXboxController(1);
+    /* Setting up bindings for necessary control of the swerve drive platform */
+    private final CommandXboxController driver = new CommandXboxController(0); // My joystick
+    private final CommandXboxController operator = new CommandXboxController(1);
 
-	private final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain;
-	// // My drivetrain
+    private final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain;
+    // // My drivetrain
 
-	private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-			.withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-			.withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric driving in open loop
+    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric driving in open loop
 
-	private final SwerveRequest.FieldCentricFacingAngle driveAt = new SwerveRequest.FieldCentricFacingAngle()
-			.withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-			.withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric driving in open loop
+    private final SwerveRequest.FieldCentricFacingAngle driveAt = new SwerveRequest.FieldCentricFacingAngle()
+            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric driving in open loop
 
-	private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-	private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
-	private final Telemetry logger = new Telemetry(MaxSpeed);
+    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+    private final Telemetry logger = new Telemetry(MaxSpeed);
 
-	/* Path follower */
-	private final SendableChooser<Command> autoChooser = drivetrain.getAutoPaths();
+    /* Path follower */
+    private final SendableChooser<Command> autoChooser = drivetrain.getAutoPaths();
+    private Mechanism mec = new Mechanism(driver, operator);
 
-	private void configureBindings() {
-		new Mechanism(driver, operator);
-		// new Vision(5892, drivetrain).start();
+    private void configureBindings() {
+        autoChooser.addOption("Cmdbased-1note", mec.pivot(0.028564453125).withTimeout(1)
+                .andThen(mec.shooter(true).alongWith(new WaitCommand(1).andThen(mec.feeder(true))).withTimeout(3)));
 
-		drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
-				drivetrain.applyRequest(() -> drive.withVelocityX(-driver.getLeftY() * MaxSpeed) // Drive forward with
-																									// negative Y
-																									// (forward)
-						.withVelocityY(-driver.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-						.withRotationalRate(-driver.getRightX() * MaxAngularRate) // Drive counterclockwise with
-																					// negative X (left)
-				));
+        // new Vision(5892, drivetrain).start();
 
-		driver.rightBumper().whileTrue(drivetrain.applyRequest(() -> driveAt
-				.withVelocityX(-driver.getLeftY() * MaxSpeed) // Drive forward with
-				// negative Y
-				// (forward)
-				.withVelocityY(-driver.getLeftX() * MaxSpeed)
-				.withTargetDirection(speakerPose.minus(drivetrain.getState().Pose).getRotation())));
+        drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
+                drivetrain.applyRequest(() -> drive.withVelocityX(-driver.getLeftY() * MaxSpeed) // Drive forward with
+                                                                                                 // negative Y
+                                                                                                 // (forward)
+                        .withVelocityY(-driver.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                        .withRotationalRate(-driver.getRightX() * MaxAngularRate) // Drive counterclockwise with
+                                                                                  // negative X (left)
+                ));
 
-		driver.a().whileTrue(drivetrain.applyRequest(() -> brake));
-		driver.b().whileTrue(drivetrain
-				.applyRequest(() -> point.withModuleDirection(new Rotation2d(-driver.getLeftY(), -driver.getLeftX()))));
+        // driver.rightBumper().whileTrue(drivetrain.applyRequest(() -> driveAt
+        // .withVelocityX(-driver.getLeftY() * MaxSpeed) // Drive forward with
+        // // negative Y
+        // // (forward)
+        // .withVelocityY(-driver.getLeftX() * MaxSpeed)
+        // .withTargetDirection(speakerPose.minus(drivetrain.getState().Pose).getRotation())));
 
-		// reset the field-centric heading on left bumper press
-		driver.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
+        // driver.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        // driver.b().whileTrue(drivetrain
+        // .applyRequest(() -> point.withModuleDirection(new
+        // Rotation2d(-driver.getLeftY(), -driver.getLeftX()))));
 
-		if (Utils.isSimulation()) {
-			drivetrain.seedFieldRelative(new Pose2d(new Translation2d(),
-					Rotation2d.fromDegrees(90)));
-		}
-		drivetrain.registerTelemetry(logger::telemeterize);
-	}
+        // reset the field-centric heading on left bumper press
+        driver.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
 
-	public RobotContainer() {
-		SmartDashboard.putData("Auto Chooser", autoChooser);
+        if (Utils.isSimulation()) {
+            drivetrain.seedFieldRelative(new Pose2d(new Translation2d(),
+                    Rotation2d.fromDegrees(90)));
+        }
+        drivetrain.registerTelemetry(logger::telemeterize);
+    }
 
-		configureBindings();
+    public RobotContainer() {
+        SmartDashboard.putData("Auto Chooser", autoChooser);
+        NamedCommands.registerCommand("PivIntakePos", mec.pivot(0.01));
+        NamedCommands.registerCommand("PivShootPos", mec.pivot(0.028564453125));
+        NamedCommands.registerCommand("PivShootPos2", mec.pivot(0.310302734375));
+        NamedCommands.registerCommand("Intake", mec.intake(true).withTimeout(1));
+        NamedCommands.registerCommand("Shoot", mec.shooter(true).alongWith(mec.feeder(true)).withTimeout(1));
 
-	}
+        configureBindings();
 
-	public Command getAutonomousCommand() {
-		return autoChooser.getSelected();
-	}
+    }
+
+    public Command getAutonomousCommand() {
+        return autoChooser.getSelected();
+    }
 }
